e29380361f201bcb07af23844eae359ca24d2f64
Mervin Lavin <mervin.lavin@gmail.com>
2015-09-11 12:47:43 +0800

    Added area for output configurations. Added basic spacing of page areas. (still dirty, need to refactor)

--- frontend/main.js
+++ frontend/main.js
@@ -1,21 +1,28 @@
 (function(){
   var app = angular.module('configurationSelector', []);
-  
+
   app.controller('FilterController', ['$scope', function($scope){
     $scope.test = 'test';
   }]);
-  
+
   app.directive('technologies', function(){
     return {
       restrict: 'E',
       templateUrl: 'technologies.html'
     };
   });
-  
+
   app.directive('systemModule', function(){
     return {
       restrict: 'E',
       templateUrl: 'systemModule.html'
+    };
+  });
+
+  app.directive('configurations', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'configurations.html'
     };
   });
 })();
