99b1d8b04fb6c46c0e16389f049a2e946aa99bbb
Mervin Lavin <mervin.lavin@gmail.com>
2015-09-11 19:02:03 +0800

    Added loading of SM configuration options.

--- frontend/js/dev/main.js
+++ frontend/js/dev/main.js
@@ -9,15 +9,30 @@ app.controller('FilterController', function($scope, ConfigurationService){
   $scope.getSmMainOptions = function() {
     var configurations = ConfigurationService.getConfigurations();
     var smMainOptions = [];
-    for( var i = 0; i < configurations.length; i++ )
-    {
-      for( var j = 0; j < configurations[i].smDeployment.length; j++ )
-      {
-        smMainOptions.push( configurations[i].smDeployment[j].fsmf.technology );
+    for (var i = 0; i < configurations.length; i++ ) {
+      var deployments = configurations[i].smDeployment;
+      for (var j = 0; j < deployments.length; j++ ) {
+        smMainOptions.push("FSMF - " + deployments[j].fsmf.technology);
       }
     }
-    console.log(smMainOptions);
-    return smMainOptions;
+    return smMainOptions.filter(onlyUnique);
+  };
+
+  $scope.getSmExtensionOptions = function() {
+    var configurations = ConfigurationService.getConfigurations();
+    var smExpansionOptions = [];
+    for (var i = 0; i < configurations.length; i++) {
+      var deployments = configurations[i].smDeployment;
+      for (var j = 0; j < deployments.length; j++) {
+        var extensionConfigurations = deployments[j].extension;
+        for (var k = 0; k < deployments[j].extension.length; k++) {
+          var technology = extensionConfigurations[k].technology;
+          var card = extensionConfigurations[k].fbbx;
+          smExpansionOptions.push(card + " - " + technology);
+        }
+      }
+    }
+    return smExpansionOptions.filter(onlyUnique);
   };
 });
 
@@ -41,3 +56,7 @@ app.directive('configurations', function() {
     templateUrl: 'configurations.html'
   };
 });
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
