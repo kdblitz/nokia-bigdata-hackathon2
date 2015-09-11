3c76f17282184b9c8f2336391f88ca7ba5e9a40e
unknown <kelee@R-QUE-DHSDTY1.nsn-intra.net>
2015-09-11 12:25:56 +0800

    Added configuration service to serve mock configurations to controller.

--- frontend/main.js
+++ frontend/main.js
@@ -1,21 +1,20 @@
-(function(){
-  var app = angular.module('configurationSelector', []);
-  
-  app.controller('FilterController', ['$scope', function($scope){
-    $scope.test = 'test';
-  }]);
-  
-  app.directive('technologies', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'technologies.html'
-    };
-  });
-  
-  app.directive('systemModule', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'systemModule.html'
-    };
-  });
-})();
+var app = angular.module('configurationSelector');
+
+app.controller('FilterController', function($scope, ConfigurationService){
+  console.log(ConfigurationService.getConfigurations())
+  $scope.test = 'test';
+});
+
+app.directive('technologies', function(){
+  return {
+    restrict: 'E',
+    templateUrl: 'technologies.html'
+  };
+});
+
+app.directive('systemModule', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'systemModule.html'
+  };
+});
