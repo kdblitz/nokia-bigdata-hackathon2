41876ffc54c0ce773b5a51efebd42719899f7dd5
Ken Lee <kdblitz@gmail.com>
2015-09-14 01:26:48 +0800

    Committed Suzette's code for upload module, with a bit of manipulation. (Frontend)

---
+++ frontend/js/dev/upload.js
@@ -0,0 +1,43 @@
+app = angular.module('configurationSelector');
+
+app.directive('fileModel', function ($parse) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var model = $parse(attrs.fileModel);
+      var modelSetter = model.assign;
+
+      element.bind('change', function(){
+        scope.$apply(function(){
+          modelSetter(scope, element[0].files[0]);
+        });
+      });
+    }
+  };
+});
+
+app.service('fileUpload', function ($http) {
+  this.uploadFileToUrl = function(file, uploadUrl){
+    var fd = new FormData();
+    fd.append('file', file);
+    console.log(fd);
+    $http.post(uploadUrl, fd, {
+      transformRequest: angular.identity,
+      headers: {'Content-Type': undefined}
+    })
+    .success(function(){
+    })
+    .error(function(){
+    });
+  }
+});
+
+app.controller('uploadCtrl', function($scope, fileUpload){
+  $scope.uploadFile = function(){
+    var file = $scope.myFile;
+    console.log('file is ' );
+    console.dir(file);
+    var uploadUrl = "/api/upload";
+    fileUpload.uploadFileToUrl(file, uploadUrl);
+  };
+});
