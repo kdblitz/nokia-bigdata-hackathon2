5857ee38f875f756177a2ea90787f34bba7c74f3
Mervin Lavin <mervin.lavin@gmail.com>
2015-09-14 14:22:55 +0800

    Hardcoded values returned by JS functions temporarily. Updated System Module buttons and look.

--- frontend/js/dev/main.js
+++ frontend/js/dev/main.js
@@ -20,60 +20,49 @@ app.controller('FilterController', function($scope, ConfigurationService){
   var configurations = ConfigurationService.getConfigurations();
   var configLen = configurations.length;
 
-  $scope.getSmMainOptions = function() {
-    var smMainOptions = [];
-    for (var i = 0; i < configLen; i++ ) {
-      var deployments = configurations[i].smDeployment;
-      for (var j = 0; j < deployments.length; j++ ) {
-        smMainOptions.push("FSMF - " + deployments[j].fsmf.technology);
-      }
-    }
-    return smMainOptions.filter(onlyUnique);
+  var bbCapacityValues = {
+    lte: [0, 1, 2, 3],
+    wcdma: [0, 3.5, 5.5, 9.5, 11.5, 15.5, 17.5],
+    gsm: [0, 24]
   };
 
+  var allFsmf = 'FSMF - All technologies';
+  var noExtension = 'No extension';
+  var allExtensions = 'FBBX - All technologies';
+  var smOptions = {
+    fsmf: [allFsmf, 'FSMF - LTE', 'FSMF - WCDMA', 'FSMF - LTE & GSM', 'FSMF - WCDMA & GSM'],
+    extensions: [noExtension, allExtensions, 'FFBA - LTE', 'FFBA - WCDMA', 'FFBA - GSM', 'FFBC - LTE', 'FBBC - WCDMA', 'FBBC - GSM']
+  };
+
+  $scope.selectedFsmf = allFsmf;
+  $scope.selectedExtension1 = noExtension;
+  $scope.selectedExtension2 = noExtension;
+
+  $scope.setSelectedFsmf = function(fsmf) {
+    $scope.selectedFsmf = fsmf;
+  }
+  $scope.setSelectedExtension1 = function(extension) {
+    $scope.selectedExtension1 = extension;
+  }
+  $scope.setSelectedExtension2 = function(extension) {
+    $scope.selectedExtension2 = extension;
+  }
+
+  $scope.getSmFsmfOptions = function() {
+    return smOptions.fsmf;
+  };
   $scope.getSmExtensionOptions = function() {
-    var smExpansionOptions = [];
-    for (var i = 0; i < configLen; i++) {
-      var deployments = configurations[i].smDeployment;
-      for (var j = 0; j < deployments.length; j++) {
-        var extensionConfigurations = deployments[j].extension;
-        for (var k = 0; k < deployments[j].extension.length; k++) {
-          var technology = extensionConfigurations[k].technology;
-          var card = extensionConfigurations[k].fbbx;
-          smExpansionOptions.push(card + " - " + technology);
-        }
-      }
-    }
-    return smExpansionOptions.filter(onlyUnique);
+    return smOptions.extensions;
   };
-
   $scope.getBBCapacityValuesForLTE = function() {
-    return [
-      { bbVal: 0 },
-      { bbVal: 1 },
-      { bbVal: 2 },
-      { bbVal: 3 }
-    ];
+    return bbCapacityValues.lte;
   }
   $scope.getBBCapacityValuesForWCDMA = function() {
-    return [
-      { bbVal: 0 },
-      { bbVal: 3.5 },
-      { bbVal: 5.5 },
-      { bbVal: 9.5 },
-      { bbVal: 11.5 },
-      { bbVal: 15.5 },
-      { bbVal: 17.5 }
-    ];
+    return bbCapacityValues.wcdma;
   }
   $scope.getBBCapacityValuesForGSM = function() {
-    return [
-      { bbVal: 0 },
-      { bbVal: 24 }
-    ];
+    return bbCapacityValues.gsm;
   }
-
-
 });
 
 app.directive('technologies', function() {
